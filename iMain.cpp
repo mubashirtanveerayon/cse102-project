f9599a66d380e312388bca06a96a451d4c30c30b
mubashirtanverayon <ayon261261skylinegtr@gmail.com>
2024-03-05 13:12:01 +0600

    update1

--- iMain.cpp
+++ iMain.cpp
@@ -1,7 +1,7 @@
-# include "iGraphics.h"
+#include "iGraphics.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 #include <time.h>
 #include <windows.h>
 #include <mmsystem.h>
@@ -14,6 +14,8 @@
 #define GAME_STATE 2
 #define TOTAL_NUMBER_OF_WORDS 37
 #define NUMBER_OF_DEMOS 4
+#define WORD_LENGTH 5
+
 int page = MAIN_MENU_STATE;
 
 int score = 0;
@@ -25,29 +27,27 @@ int remainingHints = 5;
 int typedWordIndex = -1;
 int pointerIndex = 0;
 
-int pointerX[5] = {114,167,215,265,315};
-int characterX[5] = {121,174,223,271,319};
+int pointerX[5] = {114, 167, 215, 265, 315};
+int characterX[5] = {121, 174, 223, 271, 319};
 
 int typedWordColor[3];
 int revealedIndices[5];
 
 int tutorialPageIndex = 0;
 
-int bestScore=0;
+int bestScore = 0;
 
-
-char tutorialImages[NUMBER_OF_DEMOS][20] = {"t11.bmp","t222.bmp","t33.bmp","t4.bmp"};
+char tutorialImages[NUMBER_OF_DEMOS][20] = {"t11.bmp", "t222.bmp", "t33.bmp", "t4.bmp"};
 char words[TOTAL_NUMBER_OF_WORDS][6] = {
-    "APPLE","TABLE","MUSIC","DANCE","TIGER","CHAIR","WATCH","LIGHT","PAPER","RIVER","SNAKE","EAGLE","OCEAN","HAPPY","SMILE","LAUGH","FROST","BREAD","PIZZA","QUEEN","PLANT","FRESH","GREEN","PHONE","GRAPE","HONEY","STONE","SPACE","STORM","SWEET","OTHER","CRUEL","HEAVY","LIMIT","CREST","GROUP","QUEUE"
-};
+    "APPLE", "TABLE", "MUSIC", "DANCE", "TIGER", "CHAIR", "WATCH", "LIGHT", "PAPER", "RIVER", "SNAKE", "EAGLE", "OCEAN", "HAPPY", "SMILE", "LAUGH", "FROST", "BREAD", "PIZZA", "QUEEN", "PLANT", "FRESH", "GREEN", "PHONE", "GRAPE", "HONEY", "STONE", "SPACE", "STORM", "SWEET", "OTHER", "CRUEL", "HEAVY", "LIMIT", "CREST", "GROUP", "QUEUE"};
 
-char interfaceImages[6][20] = {"interface5.bmp","interface4.bmp","interface3.bmp","interface2.bmp","interface1.bmp","interface0.bmp"};
+char interfaceImages[6][20] = {"interface5.bmp", "interface4.bmp", "interface3.bmp", "interface2.bmp", "interface1.bmp", "interface0.bmp"};
 
 // char meaning[TOTAL_NUMBER_OF_WORDS][46] = {
 //     "fruit","furniture","soundful tapestry from instruments/vocals","a particular series of graceful movements","a large wild animal from the cat family","piece of furniture for one person to sit on","look at someone/thing for a period of time","brightness/illumination allowing us to see","a thin, flat material to write or draw on","natural wide flow of fresh water to the sea","a reptile with long, thin body without legs","a large bird living on other small animals","water body covering most of the surface","feelings of pleasure, glad or satisfaction","facial expression of being glad or pleased","a cause for derision or merriment","the temperature that causes freezing","food made from flour, water, and yeast","flat and round  baked pie of Italian origin","woman eminent in rank and politics","to put or set in the ground for growth","having its original qualities unimpaired","something that is environment-friendly","device used to transfer audio, video signal","small & round green or red-colored fruit","sweet & sticky substance made by bees","concretion of earthy or mineral matter","a boundless three-dimensional extent"
 // };
 
-char similarWords[TOTAL_NUMBER_OF_WORDS][20] = {"fruit","surface","sound","move","cat","seat","timepiece","glow","sheet","stream","serpent","raptor","sea","glad","grin","chuckle","ice","loaf","pie","monarch","flora","new","verdant","device","berry","nectar","rock","void","tempest","sugary","different","brutal","weighty","edge","peak","team","line"};
+char similarWords[TOTAL_NUMBER_OF_WORDS][20] = {"fruit", "surface", "sound", "move", "cat", "seat", "timepiece", "glow", "sheet", "stream", "serpent", "raptor", "sea", "glad", "grin", "chuckle", "ice", "loaf", "pie", "monarch", "flora", "new", "verdant", "device", "berry", "nectar", "rock", "void", "tempest", "sugary", "different", "brutal", "weighty", "edge", "peak", "team", "line"};
 
 const int nextBtnX = 140;
 const int nextBtnY = 20;
@@ -58,6 +58,7 @@ int correct, initializedTutorial;
 
 void copyArray(char *dest, char *src, size_t length);
 void swap(char *a, char *b);
+int getRandomIndex(int max);
 void shuffleArray(char *str);
 void showText(void);
 void renderHUD(void);
@@ -69,60 +70,69 @@ void callReset(void);
 void showMainMenu(void);
 void showGameInterface(void);
 
-
-void iDraw() {
-	//place your drawing codes here
-	iClear();
-    if(page == MAIN_MENU_STATE){
+void iDraw()
+{
+    // place your drawing codes here
+    iClear();
+    if (page == MAIN_MENU_STATE)
+    {
         showMainMenu();
-    }else{
+    }
+    else
+    {
         showGameInterface();
     }
 }
 
 /*
-	function iMouseMove() is called when the user presses and drags the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
-void iMouseMove(int mx, int my) {
-
+    function iMouseMove() is called when the user presses and drags the mouse.
+    (mx, my) is the position where the mouse pointer is.
+    */
+void iMouseMove(int mx, int my)
+{
 }
 
 /*
-	function iMouse() is called when the user presses/releases the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
-void iMouse(int button, int state, int mx, int my) {
+    function iMouse() is called when the user presses/releases the mouse.
+    (mx, my) is the position where the mouse pointer is.
+    */
+void iMouse(int button, int state, int mx, int my)
+{
 
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
 
-        printf("x: %d,y: %d\n",mx,my);
+        printf("x: %d,y: %d\n", mx, my);
 
-		if(page == MAIN_MENU_STATE){
-			if(mx >= 107 && mx<= 347 && my <= 276 &&my >= 223){ // start button
-				page = GAME_STATE;
+        if (page == MAIN_MENU_STATE)
+        {
+            if (mx >= 107 && mx <= 347 && my <= 276 && my >= 223)
+            { // start button
+                page = GAME_STATE;
                 PlaySound("click.wav", NULL, SND_ASYNC);
-			}else if(mx >= 104 && mx<= 345 && my <= 191 &&my >= 141){ // tutorial button
-				page = GAME_STATE;
+            }
+            else if (mx >= 104 && mx <= 345 && my <= 191 && my >= 141)
+            { // tutorial button
+                page = GAME_STATE;
                 initializedTutorial = 1;
                 PlaySound("click.wav", NULL, SND_ASYNC);
-			}else if(mx >= 108 && mx<= 345 && my <= 110 && my >= 61){ // exit button
-            PlaySound("click.wav", NULL, SND_ASYNC);
-				exit(0);
-			}
-		}
-
-	}
-	
-    
+            }
+            else if (mx >= 108 && mx <= 345 && my <= 110 && my >= 61)
+            { // exit button
+                PlaySound("click.wav", NULL, SND_ASYNC);
+                exit(0);
+            }
+        }
+    }
 }
 
 /*
-	function iKeyboard() is called whenever the user hits a key in keyboard.
-	key- holds the ASCII value of the key pressed.
-	*/
-void iKeyboard(unsigned char key) {
-	if (page == MAIN_MENU_STATE || remainingLives == 0 || keyStrokeCounter == 5)
+    function iKeyboard() is called whenever the user hits a key in keyboard.
+    key- holds the ASCII value of the key pressed.
+    */
+void iKeyboard(unsigned char key)
+{
+    if (page == MAIN_MENU_STATE || remainingLives == 0 || keyStrokeCounter == 5)
         return;
 
     if (key >= '1' && key <= '5')
@@ -162,52 +172,55 @@ void iKeyboard(unsigned char key) {
         }
     }
 
-    else if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z')|| typedWordIndex < 4){
-        
+    else if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z') && typedWordIndex < 4)
+    {
 
-		// to upper case
-		if(key >= 'a' && key <= 'z')key -= 32;
+        // to upper case
+        if (key >= 'a' && key <= 'z')
+            key -= 32;
 
-		while (typedWord[++typedWordIndex] != '\0')
-		{
-			continue;
-		}
+        while (typedWord[++typedWordIndex] != '\0')
+        {
+            continue;
+        }
 
-		typedWord[typedWordIndex] = key;
+        typedWord[typedWordIndex] = key;
 
-		keyStrokeCounter++;
+        keyStrokeCounter++;
 
-		for (int i = 0; i < 5; i++)
-		{
-			if (typedWord[i] == '\0')
-			{
-				pointerIndex = i;
-				break;
-			}
-		}
-	}
+        for (int i = 0; i < 5; i++)
+        {
+            if (typedWord[i] == '\0')
+            {
+                pointerIndex = i;
+                break;
+            }
+        }
+    }
 
     if (keyStrokeCounter == 5)
         evaluate();
 
-	//place your codes for other keys here
+    // place your codes for other keys here
 }
 
 /*
-	function iSpecialKeyboard() is called whenver user hits special keys like-
-	function keys, home, end, pg up, pg down, arraows etc. you have to use
-	appropriate constants to detect them. A list is:
-	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
-	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
-	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
-	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
-	*/
-void iSpecialKeyboard(unsigned char key) {
+    function iSpecialKeyboard() is called whenver user hits special keys like-
+    function keys, home, end, pg up, pg down, arraows etc. you have to use
+    appropriate constants to detect them. A list is:
+    GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
+    GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
+    GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
+    GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
+    */
+void iSpecialKeyboard(unsigned char key)
+{
 
-	if (key == GLUT_KEY_END) {
-		exit(0);
-	}
-	//place your codes for other keys here
+    if (key == GLUT_KEY_END)
+    {
+        exit(0);
+    }
+    // place your codes for other keys here
 }
 
 void copyArray(char *dest, char *src, size_t length)
@@ -232,13 +245,21 @@ int getRandomIndex(int max)
 
 void shuffleArray(char *str)
 {
-    int size = sizeof(str) / sizeof(char);
 
-    for (int i = 0; i < size; i++)
+    int size = WORD_LENGTH;
+    int shuffled = 0;
+    do
     {
-        int randomIndex = getRandomIndex(i + 1);
-        swap(&str[i], &str[randomIndex]);
-    }
+        for (int i = 0; i < size / 2; i++)
+        {
+            int randomIndex = getRandomIndex(size);
+            swap(&str[i], &str[randomIndex]);
+        }
+
+        for(int i=0;i<size && !shuffled;i++){
+            shuffled = str[i] != words[currentWordIndex][i];
+        }
+    } while (!shuffled);
 }
 
 void evaluate()
@@ -257,7 +278,7 @@ void evaluate()
         typedWordColor[0] = 0;
         typedWordColor[1] = 255;
         typedWordColor[2] = 0;
-        PlaySound("pickupCoin3.wav", NULL, SND_ASYNC);
+        PlaySound("pickupCoin.wav", NULL, SND_ASYNC);
     }
     else
     {
@@ -276,14 +297,17 @@ void evaluate()
     {
         iResumeTimer(resetTimer);
     }
-    if(initializedTutorial){
-        if(tutorialPageIndex+1 == NUMBER_OF_DEMOS){
+    if (initializedTutorial)
+    {
+        if (tutorialPageIndex + 1 == NUMBER_OF_DEMOS)
+        {
             initializedTutorial = 0;
-        }else{
+        }
+        else
+        {
             tutorialPageIndex++;
         }
     }
-    
 }
 
 void reset()
@@ -291,9 +315,10 @@ void reset()
 
     // currentWordIndex = getRandomIndex(TOTAL_NUMBER_OF_WORDS);
     int newWord;
-    do{
+    do
+    {
         newWord = getRandomIndex(TOTAL_NUMBER_OF_WORDS);
-    }while(newWord == currentWordIndex);
+    } while (newWord == currentWordIndex);
     currentWordIndex = newWord;
 
     for (int i = 0; i < 5; i++)
@@ -325,20 +350,20 @@ void callReset()
 
 void gameOver()
 {
-    
+
     remainingLives = 5;
     remainingHints = 5;
     reset();
     iPauseTimer(gameOverTimer);
     page = MAIN_MENU_STATE;
 
-    if(score>bestScore){
+    if (score > bestScore)
+    {
         bestScore = score;
         // saving best score
-        FILE *file  = fopen("bs.txt","w");        
-        fprintf(file,"%d",bestScore);
+        FILE *file = fopen("bs.txt", "w");
+        fprintf(file, "%d", bestScore);
         fclose(file);
-
     }
     score = 0;
 }
@@ -346,37 +371,36 @@ void gameOver()
 void showText()
 {
 
-	iSetColor(255,255,255);
+    iSetColor(255, 255, 255);
 
-	char scoreStr[10];
-	sprintf(scoreStr,"%d",score);
-	iText(110,575,scoreStr,GLUT_BITMAP_TIMES_ROMAN_24);
+    char scoreStr[10];
+    sprintf(scoreStr, "%d", score);
+    iText(110, 575, scoreStr, GLUT_BITMAP_TIMES_ROMAN_24);
 
-	// char hintsStr[2];
+    // char hintsStr[2];
 
-	// sprintf(hintsStr,"%d",remainingHints);
+    // sprintf(hintsStr,"%d",remainingHints);
 
-	// iText(365,500,hintsStr,GLUT_BITMAP_TIMES_ROMAN_24);
-    
+    // iText(365,500,hintsStr,GLUT_BITMAP_TIMES_ROMAN_24);
+
     // best score
     char bestScoreStr[8];
-    sprintf(bestScoreStr,"%d",bestScore);
-    iSetColor(100,255,89);
-    iText(110,500,bestScoreStr,GLUT_BITMAP_TIMES_ROMAN_24);
-    
+    sprintf(bestScoreStr, "%d", bestScore);
+    iSetColor(100, 255, 89);
+    iText(110, 500, bestScoreStr, GLUT_BITMAP_TIMES_ROMAN_24);
 
     if (currentWordIndex < 0)
         return;
-    iSetColor(255,255,255);
-	iText(90, 420, "Misspelled Word: ", GLUT_BITMAP_TIMES_ROMAN_24);
-	iSetColor(255, 149, 28);
+    iSetColor(255, 255, 255);
+    iText(90, 420, "Misspelled Word: ", GLUT_BITMAP_TIMES_ROMAN_24);
+    iSetColor(255, 149, 28);
     iText(280, 420, misspelledWord, GLUT_BITMAP_TIMES_ROMAN_24);
-	iSetColor(255,255,255);
-    iText(170,370,"Hint :",GLUT_BITMAP_TIMES_ROMAN_24);
+    iSetColor(255, 255, 255);
+    iText(170, 370, "Hint :", GLUT_BITMAP_TIMES_ROMAN_24);
     iSetColor(255, 231, 27);
-    iText(245,370,similarWords[currentWordIndex],GLUT_BITMAP_TIMES_ROMAN_24);
+    iText(245, 370, similarWords[currentWordIndex], GLUT_BITMAP_TIMES_ROMAN_24);
 
-	iSetColor(typedWordColor[0], typedWordColor[1], typedWordColor[2]);
+    iSetColor(typedWordColor[0], typedWordColor[1], typedWordColor[2]);
 
     for (int i = 0; i < 5; i++)
     {
@@ -396,21 +420,21 @@ void showText()
 
     if (remainingLives == 0)
     {
-        iShowBMP(0,0,"gameOver2.bmp");
+        iShowBMP(0, 0, "gameOver2.bmp");
     }
 }
 
 void renderHUD()
 {
 
-	for (int i = 0; i < remainingLives; i++)
+    for (int i = 0; i < remainingLives; i++)
     {
         iShowBMP2(390 - i * 35, 570, "heart1.bmp", 0);
     }
 
-	iShowBMP2(pointerX[pointerIndex], 150, "arrow.bmp", 0);
+    iShowBMP2(pointerX[pointerIndex], 150, "arrow.bmp", 0);
 
-	if (typedWordIndex == 4)
+    if (typedWordIndex == 4)
     {
         iSetColor(typedWordColor[0], typedWordColor[1], typedWordColor[2]);
         if (correct)
@@ -424,40 +448,41 @@ void renderHUD()
             iShowBMP2(390 - remainingLives * 35, 573, "cross.bmp", 0);
         }
     }
-    if(initializedTutorial){
-        iShowBMP(0,0,tutorialImages[tutorialPageIndex]);
+    if (initializedTutorial)
+    {
+        iShowBMP(0, 0, tutorialImages[tutorialPageIndex]);
     }
-
 }
 
-
-void showMainMenu(void){
-    iShowBMP(0,0,"mainmenu2.bmp");
+void showMainMenu(void)
+{
+    iShowBMP(0, 0, "mainmenu2.bmp");
 }
 
-void showGameInterface(void){
-	iShowBMP(0,0,interfaceImages[5-remainingHints]);
-	showText();
-	renderHUD();
+void showGameInterface(void)
+{
+    iShowBMP(0, 0, interfaceImages[5 - remainingHints]);
+    showText();
+    renderHUD();
 }
 
-
-
-int main() {
+int main()
+{
 
     // getting best score
-    FILE *file=fopen("bs.txt","r");
-    if(file == NULL){
-        file = fopen("bs.txt","w");
-        fprintf(file,"0");
+    FILE *file = fopen("bs.txt", "r");
+    if (file == NULL)
+    {
+        file = fopen("bs.txt", "w");
+        fprintf(file, "0");
         fclose(file);
-        file = fopen("bs.txt","r");
+        file = fopen("bs.txt", "r");
     }
-    fscanf(file,"%d",&bestScore);
+    fscanf(file, "%d", &bestScore);
     fclose(file);
 
-	long long int seed = 1705415488;//time(NULL); // 1705415488
-    printf("Seed: %lld",seed);
+    long long int seed = 1705415488; // time(NULL); // 1705415488
+    printf("Seed: %lld", seed);
 
     srand(seed);
     reset();
@@ -473,5 +498,4 @@ int main() {
 
     iInitialize(WIDTH, HEIGHT, "Spelling Master");
     return 0;
-
 }
